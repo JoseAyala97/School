1d97b0931b2d116c7860d0fa44e6423dad7191bb
Jose Ayala <joseayala45@outlook.es>
2024-05-15 23:30:42 -0500

    fix: fixMigration - implementing intermediate table.

---
+++ Migrations/20240516042340_FixRelation.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Student_StudentId",
+                table: "Course");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Course_Teacher_TeacherId",
+                table: "Course");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Course_StudentId",
+                table: "Course");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Course_TeacherId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Course");
+
+            migrationBuilder.DropColumn(
+                name: "TeacherId",
+                table: "Course");
+
+            migrationBuilder.CreateTable(
+                name: "CourseToStudent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    TeacherId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseToStudent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CourseToStudent_Course_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Course",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseToStudent_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseToStudent_Teacher_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teacher",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseToStudent_CourseId",
+                table: "CourseToStudent",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseToStudent_StudentId",
+                table: "CourseToStudent",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseToStudent_TeacherId",
+                table: "CourseToStudent",
+                column: "TeacherId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseToStudent");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeacherId",
+                table: "Course",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_StudentId",
+                table: "Course",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Course_TeacherId",
+                table: "Course",
+                column: "TeacherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Student_StudentId",
+                table: "Course",
+                column: "StudentId",
+                principalTable: "Student",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Course_Teacher_TeacherId",
+                table: "Course",
+                column: "TeacherId",
+                principalTable: "Teacher",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
