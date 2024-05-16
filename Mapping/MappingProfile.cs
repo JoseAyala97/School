3378430720398e36d62d7ce3732526c8d094ff9f
Jose Ayala <joseayala45@outlook.es>
2024-05-15 23:32:50 -0500

    add: mapping (automapper)

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using School.Models.Entities;
+using School.Services.ViewModel;
+
+namespace School.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Student, StudentVm>();
+        
+        }
+    }
+}
