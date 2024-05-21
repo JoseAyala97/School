c5fb01f036d627c471946ba4f47da99a1791c4a5
Jose Ayala <joseayala45@outlook.es>
2024-05-20 23:29:49 -0500

    add: StudentController

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using School.Contract.Services;
+using School.Services.Request;
+using School.Services.ViewModel;
+
+namespace School.Controllers
+{
+    [ApiController]
+    [Route("api/student")]
+    public class StudentController : ControllerBase
+    {
+        private readonly IStudentServices _studentServices;
+
+        public StudentController(IStudentServices studentServices)
+        {
+            _studentServices = studentServices;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<StudentVm>>> Get()
+        {
+            var students = await _studentServices.Get();
+            return Ok(students);
+        }
+        [HttpGet("ById")]
+        public async Task<ActionResult<StudentVm>> GetById(int id)
+        {
+            var student = await _studentServices.GetById(id);
+            return Ok(student);
+        }
+        [HttpPost]
+        public async Task<ActionResult<StudentVm>> Post(StudentRequest request)
+        {
+            var student = await _studentServices.Post(request);
+            return Ok(student);
+        }
+    }
+}
+
+
