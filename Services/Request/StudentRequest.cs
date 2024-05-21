3da1496f79e840676b57407d1c8d12a84c70bab9
Jose Ayala <joseayala45@outlook.es>
2024-05-20 23:28:10 -0500

    add: StudentRequest add: StudentServices (Get, GetById, Post)

---
+++ Services/Request/StudentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace School.Services.Request
+{
+    public class StudentRequest
+    {
+        public string? Name { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+    }
+}
--- Services/StudentServices.cs
+++ Services/StudentServices.cs
@@ -1,5 +1,8 @@
-﻿using School.Contract.Repositories;
+﻿using AutoMapper;
+using School.Contract.Repositories;
 using School.Contract.Services;
+using School.Models.Entities;
+using School.Services.Request;
 using School.Services.ViewModel;
 
 namespace School.Services
@@ -7,13 +10,60 @@ namespace School.Services
     public class StudentServices : IStudentServices
     {
         private readonly IStudentRepository _studentRepository;
-        public StudentServices(IStudentRepository studentRepository)
+        private readonly IMapper _mapper;
+        public StudentServices(IStudentRepository studentRepository, IMapper mapper)
         {
             _studentRepository = studentRepository;
+            _mapper = mapper;
         }
-        public async Task<StudentVm> GetStudents()
+        public async Task<IReadOnlyList<StudentVm>> Get()
         {
+            try
+            {
+                var students = await _studentRepository.GetAsync(s => true);
+                var studentsResponse = _mapper.Map<IReadOnlyList<StudentVm>>(students);
+                return studentsResponse;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public async Task<StudentVm> GetById(int id)
+        {
+            try
+            {
+                var student = await _studentRepository.GetByIdAsync(id);
+                var studentResponse = _mapper.Map<StudentVm>(student);
+                return studentResponse;
+            }
+            catch (Exception)
+            {
 
+                throw;
+            }
+        }
+        public async Task<StudentVm> Post(StudentRequest request)
+        {
+            try
+            {
+                var student = new Student
+                {
+                    Name = request.Name,
+                    LastName = request.LastName,
+                    Age = request.Age
+                };
+
+                await _studentRepository.InsertAsync(student);
+                var studentVm = _mapper.Map<StudentVm>(student);
+                return studentVm;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
