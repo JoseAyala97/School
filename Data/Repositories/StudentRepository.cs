8fb1cd6ab11294af89298807d3c1e23188de26f7
Jose Ayala <joseayala45@outlook.es>
2024-05-20 20:10:50 -0500

    add: StudentRepository.

--- Data/Repositories/StudentRepository.cs
+++ Data/Repositories/StudentRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using School.Contract.Repositories;
 using School.Data.context;
 using School.Models.Entities;
@@ -21,6 +23,25 @@ namespace School.Data.Repositories
                 query = include(query);
 
             return await query.ToListAsync();
+        }
+        public async Task<Student> FirstOrDefault(Expression<Func<Student, bool>> predicate, Func<IQueryable<Student>, IQueryable<Student>> include = null)
+        {
+            IQueryable<Student> query = _applicationDbContext.Set<Student>().AsQueryable();
+            if (include != null)
+                query = include(query);
+
+            return await query.FirstOrDefaultAsync(predicate);
+        }
+        public async Task<Student> GetByIdAsync(int id)
+        {
+            return await _applicationDbContext.Students.FindAsync(id);
+        }
+        public async Task<Student> InsterAsync(Student student)
+        {
+            _applicationDbContext.Students.Add(student);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return student;
         }
     }
 }
